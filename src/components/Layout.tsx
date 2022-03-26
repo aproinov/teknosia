024e11e18444d8e2726940184ed424df70ae1a75
aproinov <98790856+aproinov@users.noreply.github.com>
2022-03-26 07:01:15 +0700

    Update Layout.tsx

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -11,7 +11,8 @@ export default function Layout({ children }: Props) {
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="manifest" href="/site.webmanifest" />
-        <link rel="apple-touch-icon" href="/icon.png" />
+        <link rel="icon" href="/favicons.ico"></link>
+        <link rel="apple-touch-icon" href="/favicon.ico" />
         <meta name="theme-color" content="#fff" />
         <link href="https://fonts.googleapis.com/css?family=Righteous%7CMerriweather:300,300i,400,400i,700,700i" rel="stylesheet" />
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossOrigin="() => {
