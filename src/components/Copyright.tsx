22ac288b8f697fc8d6f6c8d886799ff205fede3a
aproinov <98790856+aproinov@users.noreply.github.com>
2022-03-26 07:27:39 +0700

    Update Copyright.tsx

--- src/components/Copyright.tsx
+++ src/components/Copyright.tsx
@@ -2,7 +2,7 @@ export default function Copyright() {
   return (
     <>
       <p><div>
-                        <a href="/privacy">Privacy</a> &nbsp;&nbsp;/&nbsp;&nbsp; <a href="/terms">Terms</a> &nbsp;&nbsp;/&nbsp;&nbsp; <a href="/disclaimer">Disclaimer</a>
+                        <a href="/privacy">Privacy</a> &nbsp;&nbsp;&nbsp;&nbsp;/&nbsp;&nbsp;&nbsp;&nbsp; <a href="/terms">Terms</a> &nbsp;&nbsp;&nbsp;&nbsp;/&nbsp;&nbsp;&nbsp;&nbsp; <a href="/disclaimer">Disclaimer</a>
                     </div><br />&copy; 2022 - Teknosia</p>
       <style jsx>
         {`
